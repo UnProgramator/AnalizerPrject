ca1b66e9d4843af706161aa6f952986ce6b445b8
Pescaru Alexandru <apescaru11@yahoo.com>
2023-04-14 19:21:18 +0300

    implemented UIH

--- Analizer/Analizer/AntipatternsDetection/Implementations/UnhealthyInheritanceHierarchyDetector.cs
+++ Analizer/Analizer/AntipatternsDetection/Implementations/UnhealthyInheritanceHierarchyDetector.cs
@@ -1,17 +1,105 @@
-﻿using DRSTool.Analizer.Models;
+﻿using DRSTool.Analizer.AntipatternsDetection.Utilities;
+using DRSTool.Analizer.Models;
 using DRSTool.CommonModels;
 
 namespace DRSTool.Analizer.AntipatternsDetection.Implementations;
 
 internal class UnhealthyInheritanceHierarchyDetector : IAntipatternDetector
 {
-    public void detect(AnalizerModel dataModel, ResultModel results)
+    private InheritanceTree tree;
+
+    public UnhealthyInheritanceHierarchyDetector(InheritanceTree tree)
     {
-        throw new NotImplementedException();
+        this.tree = tree;
     }
 
-    public void setTreshold(string name, int value)
-    {
-        throw new NotImplementedException();
+    public UnhealthyInheritanceHierarchyDetector() 
+    { 
+        this.tree = new InheritanceTree();
     }
+
+    public void detect(AnalizerModel dataModel, ResultModel results)
+    {
+        if (!tree.isInitialized())
+            tree.init(dataModel);
+
+        base_depend_on_derived(dataModel, results);
+        class_depends_on_base_and_derived(dataModel, results);
+    }
+
+    private void base_depend_on_derived(AnalizerModel dataModel, ResultModel results)
+    {
+        for(int b=0; b<dataModel.Entities.Length; b++)
+        {
+            for(int d=0; d< dataModel.Entities.Length; d++)
+            {
+                //the quick checks
+                if (dataModel.SRelations[b, d] == null) continue; // no structural dependency from the "potential" parent to the derived
+                if (!dataModel.SRelations[b, d].Properties.ContainsKey("dependency")) continue; //no dependency involved () ??? this may be removed?
+
+                if (dataModel.SRelations[d, b] == null) continue; // no structural relation at all; for i==j M[i,j] is null by default
+                
+                //the costly checks for indirect inheritance
+
+                if (!tree.inherits(d, b)) continue; // check for indirect inheritance
+
+                var _base = dataModel.Entities[b].Name;
+
+                var value = new Dictionary<string, object> {
+                    { "antipattern-type", "unhealthy-inheritance" },
+                    { "type", "base-depends-on-derived" }
+                };
+
+                results.add(_base, value);
+
+                break; // there may be problems if it is the case a base depends on multiple deriveds. If ti depends on one or on more is the same
+            } 
+        }
+    }
+
+    private void class_depends_on_base_and_derived(AnalizerModel dataModel, ResultModel results)
+    {
+        for (int c = 0; c < dataModel.Entities.Length; c++)
+        {
+            for (int b = 0; b < dataModel.Entities.Length; b++)
+            {
+                if (dataModel.SRelations[c, b] == null) continue; // no structural dependency of class c on the potential base class
+
+                // class c inherits from b, case already threated
+                // but what if a class depends on it's base and on of it's base derivedes from another derivation sub-tree!
+                // if (dataModel.SRelations[c, b].Properties.ContainsKey("inheritance")) continue; 
+                
+
+                foreach(var d in tree.getChildren(b))
+                {
+                    //if c and d are the same, we continue; the diagonal must be empty
+                    if (dataModel.SRelations[c, b] == null) continue; // no UIH here
+
+                    var _base = dataModel.Entities[b].Name;
+                    var _class = dataModel.Entities[c].Name;
+                    var _derived = dataModel.Entities[d].Name;
+
+                    var value_base = new Dictionary<string, object> {
+                        { "antipattern-type", "unhealthy-inheritance" },
+                        { "type", "class-depends-on-base-and-derived" },
+                        { "class", _class }
+                    };
+
+                    results.add(_base, value_base);
+
+                    var value_class = new Dictionary<string, object> {
+                        { "antipattern-type", "unhealthy-inheritance" },
+                        { "type", "class-depends-on-base-and-derived" },
+                        { "base-class", _base }
+                    };
+
+                    results.add(_class, value_class);
+
+                    break; //it's enough if it occured for a derived of the analized base class
+                }
+            }
+        }
+    }
+
+
 }
---
+++ Analizer/Analizer/AntipatternsDetection/Utilities/InheritanceTree.cs
@@ -0,0 +1,126 @@
+﻿using DRSTool.CommonModels;
+using System.Collections.Generic;
+
+namespace DRSTool.Analizer.AntipatternsDetection.Utilities;
+
+class InheritanceTree
+{
+    private IDictionary<int, Node>? data;
+
+    public bool isInitialized() { return data != null; }
+
+    public int count() { return data != null ? data.Count() : 0; }
+
+    public bool inherits(int _derived, int _base)
+    {
+        if (data == null)
+            throw new Exception("Inheritance Tree used but not initialized!");
+
+        if(!data.ContainsKey(_derived) || !data.ContainsKey(_base)) //derived or base not in a hierarchy
+            return false;
+
+        var children = data[_base].childrens;
+
+        if (children == null)
+            return false;
+
+        if (children.Contains(_derived))
+            return true;
+
+        return false;
+    }
+
+    public ISet<int> getChildren(int baseIdx)
+    {
+        if (data == null)
+            throw new Exception("Inheritance Tree used but not initialized!");
+
+        var childrens = data[baseIdx].childrens;
+
+        return childrens != null ? childrens : new SortedSet<int>();
+    }
+
+    public void init(AnalizerModel source)
+    {
+        if (data != null)
+            throw new Exception("Reinitialization atempt");
+        data = new Dictionary<int, Node>();
+
+        for(int d=0; d<= source.Entities.Length; d++)
+            for(int b=0; b< source.Entities.Length; b++)
+                if( source.SRelations[d, b] != null &&
+                    source.SRelations[d, b].Properties.ContainsKey("inheritance"))
+                {
+                    if (!data.ContainsKey(d))
+                        data.Add(d, new Node(b));
+                    data[d].addParent(b);
+
+                    if(!data.ContainsKey(b))
+                        data.Add(b, new Node(null));
+                    data[b].addChildren(d); 
+                }
+
+        foreach((var key, var value) in data){
+            if (value.parents == null) // the update is from the root of the tree to the leaves.
+                update(key);
+        }
+    }
+
+    private void update(int index)
+    {
+        if (data == null)
+            return;
+
+        Node crt = data[index];
+
+        if (crt.isMerged)
+            return;
+
+        if (crt.childrens == null)
+            return;
+
+        var initSet = new SortedSet<int>(crt.childrens);
+
+        foreach (int chldIdx in initSet)
+        {
+            update(chldIdx);
+            var children = data[chldIdx].childrens;
+            if (children != null)
+                crt.childrens.UnionWith(children);
+        }
+              
+
+        crt.isMerged = true;
+    }
+}
+
+internal class Node
+{
+    //public int index; this is the dictionary key
+    public SortedSet<int>? parents = null;  // parent class and interfaces; direct inheritance only
+    public SortedSet<int>? childrens = null; // derived classes/interfaces; both direct and indirect inheritance
+
+    public bool isMerged = false;
+
+    public Node(int? parent)
+    {
+        if (parent != null) {
+            parents = new SortedSet<int>();
+            parents.Add((int)parent);
+        }
+    }
+
+    public void addChildren(int childrenIdx)
+    {
+        if(childrens == null)
+            childrens = new SortedSet<int>();
+        childrens.Add(childrenIdx);
+    }
+
+    public void addParent(int parentIdx)
+    {
+        if (childrens == null)
+            childrens = new SortedSet<int>();
+        childrens.Add(parentIdx);
+    }
+}
